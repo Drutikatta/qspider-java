0f10bc98b9154db5dcb552cb5a55f517abf067ea
“Drutikatta” <drutikatta26@gmail.com>
2026-01-21 10:58:49 +0530

    patterns

---
+++ loops/src/main/java/org/example/pattern6.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        int num=6;
+        for (int i=1;i<=num;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+
+//                if ( i>=j)
+//                    System.out.print("* ");
+//                else
+//                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
--- loops/src/main/java/org/example/pattren5.java
+++ loops/src/main/java/org/example/pattren5.java
@@ -2,22 +2,21 @@ package org.example;
 
 public class pattren5 {
     public static void main(String[] args) throws Exception{
-//*           *
-//  *       *
-//    *   *
-//      *
-//    *   *
-//  *       *
-//*           *
+        //*           *
+        //  *       *
+        //    *   *
+        //      *
+        //    *   *
+        //  *       *
+        //*           *
         int num=7;
         for (int i=1;i<=num;i++){
             for (int j=1;j<=num;j++){
-                if (i==j || i+j==num+1){
+                if (i==j || i+j==num+1)
                     System.out.print("* ");
-                    Thread.sleep(100);
-                }
                 else
                     System.out.print("  ");
+                Thread.sleep(100);
             }
             System.out.println();
         }
